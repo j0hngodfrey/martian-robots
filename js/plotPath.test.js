cdf78eeb6a9090ecb81da74b5081b71ec869c746
John Godfrey <jgodfr04@dcs.bbk.ac.uk>
2022-06-02 20:59:44 +0100

    Added tests for more edge cases

--- js/plotPath.test.js
+++ js/plotPath.test.js
@@ -94,3 +94,60 @@ describe("Robot is given input of the maximum sizes specified", () => {
     });
   });
 });
+
+describe("Robot goes round the outside of a grid where previously a robot has fallen off every edge (in both directions on each corner)", () => {
+  it("should return its final location and orientation as usual", () => {
+    const result = plotPath(
+      {
+        instructions: "FFFLFFFLFFFLFF",
+        orientation: "E",
+        startX: "0",
+        startY: "0",
+      },
+      3,
+      3,
+      [
+        { x: 0, y: 0, orientation: "S", isLost: true },
+        { x: 0, y: 0, orientation: "W", isLost: true },
+        { x: 0, y: 1, orientation: "W", isLost: true },
+        { x: 0, y: 2, orientation: "W", isLost: true },
+        { x: 0, y: 3, orientation: "W", isLost: true },
+        { x: 0, y: 3, orientation: "N", isLost: true },
+        { x: 1, y: 3, orientation: "N", isLost: true },
+        { x: 2, y: 3, orientation: "N", isLost: true },
+        { x: 3, y: 3, orientation: "N", isLost: true },
+        { x: 3, y: 3, orientation: "E", isLost: true },
+        { x: 3, y: 2, orientation: "E", isLost: true },
+        { x: 3, y: 1, orientation: "E", isLost: true },
+        { x: 3, y: 0, orientation: "E", isLost: true },
+        { x: 3, y: 0, orientation: "S", isLost: true },
+        { x: 2, y: 0, orientation: "S", isLost: true },
+        { x: 1, y: 0, orientation: "S", isLost: true },
+      ]
+    );
+    expect(result).toEqual({
+      isLost: false,
+      orientation: "S",
+      x: 0,
+      y: 1,
+    });
+  });
+});
+
+describe("Robot repeatedly tries to go off an edge where a previous robot fell off", () => {
+  it("should stay in the original position", () => {
+    const result = plotPath(
+      {
+        startX: "3",
+        startY: "3",
+        orientation: "N",
+        instructions: "FFFFFFFFFFFFFFFFFFFFFFFFFFF",
+      },
+      5,
+      3,
+      [{ x: 3, y: 3, orientation: "N", isLost: true }]
+    );
+
+    expect(result).toEqual({ isLost: false, orientation: "N", x: 3, y: 3 });
+  });
+});
